52633bc7aa1a703085a7c3bb4e779ba42d8d3ff6
bunnyy <36611570+bunnyyTheFreak@users.noreply.github.com>
2020-07-15 19:42:30 +0000

    tulip: define missing local path
    Signed-off-by:- bunnyyTheFreak <hsinghalk@yahoo.com>

--- freaky_tulip.mk
+++ freaky_tulip.mk
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+LOACL_PATH := device/xiaomi/tulip
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
